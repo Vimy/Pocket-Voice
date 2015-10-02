f7577b12e11e6438fb97cf7af48b618cb50c45e2
Matthias <matthias.vermeulen@gmail.com>
2015-10-03 00:45:31 +0200

    Adding pocket api

---
+++ Pocket Voice/PocketItem.h
@@ -0,0 +1,13 @@
+//
+//  PocketItem.h
+//  Pocket Voice
+//
+//  Created by Matthias Vermeulen on 3/10/15.
+//  Copyright © 2015 Noizy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PocketItem : NSObject
+
+@end
---
+++ Pocket Voice/PocketItem.m
@@ -0,0 +1,13 @@
+//
+//  PocketItem.m
+//  Pocket Voice
+//
+//  Created by Matthias Vermeulen on 3/10/15.
+//  Copyright © 2015 Noizy. All rights reserved.
+//
+
+#import "PocketItem.h"
+
+@implementation PocketItem
+
+@end
