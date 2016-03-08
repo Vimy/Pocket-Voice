4ce733628417e2e3a18e36a2c7abe230d607dfbb
Matthias <matthias.vermeulen@gmail.com>
2016-03-08 16:54:38 +0100

    layout changes settingsViewController

---
+++ Pocket Voice/ActivityViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ActivityViewController.swift
+//  Pocket Voice
+//
+//  Created by Matthias Vermeulen on 8/03/16.
+//  Copyright © 2016 Noizy. All rights reserved.
+//
+
+import Foundation
---
+++ Pocket Voice/Colors.h
@@ -0,0 +1,13 @@
+//
+//  Colors.h
+//  Pocket Voice
+//
+//  Created by Matthias Vermeulen on 8/03/16.
+//  Copyright © 2016 Noizy. All rights reserved.
+//
+
+#ifndef Colors_h
+#define Colors_h
+
+
+#endif /* Colors_h */
